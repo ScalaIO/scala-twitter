a432848257767f5d3816d134380b772ab6ed0a10
François Sarradin <fsarradin@gmail.com>
2017-11-08 15:24:18 +0100

    add tweet deserialization from json (FIXME but doesn't work)

--- src/main/scala/fr/scalaio/twitter/FileSupport.scala
+++ src/main/scala/fr/scalaio/twitter/FileSupport.scala
@@ -3,22 +3,25 @@ package fr.scalaio.twitter
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-import com.danielasfregola.twitter4s.http.serializers.CustomSerializers
+import com.danielasfregola.twitter4s.http.serializers._
 import de.heikoseeberger.akkahttpjson4s.Json4sSupport
 import org.json4s.native.Serialization
 import org.json4s.{DefaultFormats, Formats}
 
 trait FileSupport extends Json4sSupport {
 
-  object TwitterDefaultFormats extends DefaultFormats {
+  implicit object TwitterDefaultFormats extends DefaultFormats {
     override def dateFormatter: SimpleDateFormat =
       new SimpleDateFormat("EEE MMM dd HH:mm:ss ZZZZ yyyy", Locale.US)
   }
 
   val json4sFormats: Formats =
-    TwitterDefaultFormats ++ CustomSerializers.all
+    TwitterDefaultFormats ++ List(LocalDateSerializer, DisconnectionCodeSerializer)
 
   def toJson[T <: AnyRef](value: T): String =
-    Serialization.write(value)(json4sFormats)
+    Serialization.write(value)
+
+  def fromJson[T : Manifest](json: String): T =
+    Serialization.read[T](json)
 
 }
